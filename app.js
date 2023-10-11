38674eca8df519ff00e97cbe04655a70dbb4e9a9
CH V V GOPAL RAJU KALIDINDI <S559007@NWMISSOURI.EDU>
2023-10-11 11:28:21 -0500

    defined error route
    defined error route

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ app.get('/', (req, res) => {
 
 // configure routes
 app.use('/api', routes);
+// undefined route for get
+app.get('*', (req, res) => {
+    res.status(404).send(`undefined get request: ${req.url}`);
+});
+
 
 // start server on user defined port
 
