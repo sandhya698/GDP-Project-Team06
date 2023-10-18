35be47b4f6cef7528ef2105b8f033bbd2cc59620
03-eng <aparnareddy169@gmail.com>
2023-10-18 09:33:32 -0500

    hashing password during registration

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 /* By default mongoose with add 's' at the end of collection
  * to nullify that use pluralize as null */ 
@@ -42,6 +43,23 @@ const userSchema = mongoose.Schema({
         type: Date,
     }
 });
+
+/* This is a default method 'pre' which runs everytime before saving 
+ * into the database. If password is changed hash the password.
+ * Finaly call next() method which continues the process.
+ */
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        try {
+            this.password = await bcrypt.hash(this.password, 12);
+            this.cpassword = await bcrypt.hash(this.cpassword, 12);
+        } catch (error) {
+            // Handle the error appropriately
+            console.error('Error occurred during password hashing:', error);
+        }
+    }
+    next();
+})
 
 // creating a model and exporting
 const Users = mongoose.model('Users', userSchema);
