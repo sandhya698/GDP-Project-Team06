b281267e0ebab09cedc180e3ff4198f49293ace7
praveennarni <praveenbabunarni1999@gmail.com>
2023-10-23 09:43:42 -0500

    React error route added

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Register } from './pages/Register';
 import { Login } from './pages/Login';
+import { Error } from './pages/Error';
 
 export default function App() {
-  return (
-      <BrowserRouter>
-          <Routes>
+    return (
+        <BrowserRouter>
+            <Routes>
                 <Route path='/'         element={<Home />} />
                 <Route path='/login'    element={<Login />} />
                 <Route path='/register' element={<Register />} />
-          </Routes>
-      </BrowserRouter>
-  );
+                <Route path='*'         element={<Error />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
