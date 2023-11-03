ca4f31f12de4a96dc159d30a1c2d5c4e08de2663
Poojasriramineni <poojasri4399@gmail.com>
2023-11-03 09:28:02 -0500

    fixed toasts on login and registration

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,20 @@ import { Home } from './pages/Home';
 import { Register } from './pages/Register';
 import { Login } from './pages/Login';
 import { Error } from './pages/Error';
+import { ToastContainer } from 'react-toastify';
 
 export default function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/'         element={<Home />} />
-                <Route path='/login'    element={<Login />} />
-                <Route path='/register' element={<Register />} />
-                <Route path='*'         element={<Error />} />
-            </Routes>
-        </BrowserRouter>
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/'         element={<Home />} />
+                    <Route path='/login'    element={<Login />} />
+                    <Route path='/register' element={<Register />} />
+                    <Route path='*'         element={<Error />} />
+                </Routes>
+            </BrowserRouter>
+            <ToastContainer />
+        </>
     );
 };
