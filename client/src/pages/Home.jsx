5ed139992bb792c1aa85b7403e67fef5b72ee6fe
praveennarni <praveenbabunarni1999@gmail.com>
2023-11-10 12:31:08 -0600

    home page authentication

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import NavigationBar from '../components/NavigationBar'
+import axios from 'axios';
+import { authenticate } from '../utils/ApiRoutes';
+import { useNavigate } from 'react-router-dom';
 
 export const Home = () => {
+
+  const [currentUser, setCurrentUser] = useState(undefined);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  const checkUserAuthentication = useCallback(async () => {
+    try {
+      console.log('autnentcaitng')
+      const res = await axios.get(authenticate, {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+      console.log(res.data);
+      setCurrentUser(res.data.user);
+      setLoading(false);
+    }
+    catch (err) {
+      console.log(err.response.data);
+      setLoading(false);
+      if (!err.response.data.status) {
+        navigate('/login');
+      }
+    }
+  }, [navigate]);
+
+  useEffect(() => {
+    checkUserAuthentication();
+  }, [checkUserAuthentication, navigate]);
+  
   return (
     <>
       <NavigationBar />
-      <div><p>How are you feeling today?</p></div>
+      {
+        loading ?
+          (
+            <p>Loading...</p>
+          ) :
+          (
+            <div><p>How are you feeling today { currentUser.name }?</p></div>
+          )
+      }
     </>
   )
 }
