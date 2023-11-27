42826fd676e510e31d118233ddd99819f5ef8fc4
S560460 <S560460@nwmissouri.edu>
2023-11-27 11:29:13 -0600

    validations added to donor request route

--- server/src/controllers/historyController.js
+++ server/src/controllers/historyController.js
@@ -1,8 +1,24 @@
 const DonorRequestHistory = require("../models/donorRequestModel");
+const bloodGroups = ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"];
+const typeList = ["donate", "request"];
 
 module.exports.donorRequest = async (req, res) => {
     const { bloodGroup, quantity, userId, disease } = req.body;
     const type = req.params.type;
+
+    // some validations
+    if (!typeList.includes(type)) {
+        return res.status(422).json({
+            success: false,
+            message: `Invalid request = ${type}`
+        });
+    }
+    else if (!bloodGroups.includes(bloodGroup)) {
+        return res.status(422).json({
+            success: false,
+            message: `Invalid blood group ${bloodGroup}`
+        });
+    }
 
     try {
         const donorHistRec = new DonorRequestHistory({ bloodGroup, quantity, type, disease, status: 'pending', donor: userId });
