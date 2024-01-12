46d6a8a2aa9eaae910beacc8de0ece48f58b7ff0
S560460 <S560460@nwmissouri.edu>
2024-01-12 17:59:33 -0600

    row count and added some styling

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,50 +1,49 @@
 import React from 'react';
 import { Card, Row, Col } from 'react-bootstrap';
+import styled from 'styled-components';
 
-const MyCard = ({ count, height }) => {
+const MyCard = ({ count, height, rowCount }) => {
   
   const CustomCard = () => {
     return (
-        <Card style={{ height }}>
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a longer card with supporting text below as a natural
-            lead-in to additional content. This content is a little bit
-            longer.
-          </Card.Text>
-        </Card.Body>
+      <Card border='primary' className='flex-row shadow' style={{ height }}>
+        {/* <Row> */}
+          <Col md={8}>
+            <Card.Body>
+              <Card.Title>title</Card.Title>
+              <Card.Text>
+                Body
+              </Card.Text>
+            </Card.Body>
+          </Col>
+          <Col md={4}>
+            <Icon>
+              <i className="fas fa-tint"></i>
+            </Icon>
+          </Col>
+        {/* </Row> */}
       </Card>
     );
   }
 
   return (
     <>
-      <Row xs={1} md={4} className="g-4 mt-0">
+      <Row xs={1} md={rowCount} className="g-4 mt-0">
         {Array.from({ length: count }).map((_, idx) => (
           <Col key={idx}>
             <CustomCard />
           </Col>
         ))}
       </Row>
-      {/* <Row xs={1} md={4} className="g-4 mt-0">
-      {Array.from({ length: count }).map((_, idx) => (
-        <Col key={idx}>
-          <Card>
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
-    </Row> */}
     </>
   );
 };
+
+const Icon = styled.div`
+  align-items:center;
+  justify-content: center;
+  color: red;
+  font-size: 3rem;
+`
 
 export default MyCard;
