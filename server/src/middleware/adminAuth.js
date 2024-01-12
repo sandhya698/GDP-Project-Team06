a4ce62291bb838a6ee511bb8115daf4936bd07cb
Poojasriramineni <poojasri4399@gmail.com>
2024-01-12 17:33:15 -0600

    created middleware for admin, added authetication

---
+++ server/src/middleware/adminAuth.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken');
+const Users = require('../models/userModel');
+
+const adminAuth = async (req,res,next) => {
+    try {
+        
+        // token = 'bloodToken' is stored in request
+        const token = req.cookies.bloodToken;
+        
+        // verifying the token using the secret key
+        const verifyUser = jwt.verify(token, process.env.SECRET_KEY);
+        
+        // verifing user with the token saved in cookie with our secret key
+        const user = await Users.findOne({ _id: verifyUser._id, "tokens.token": token });
+
+        if (!user) {
+            throw new Error("User not authenticated");
+        }
+
+        if (user.userType !== 'admin') {
+            throw new Error("You are not an Admin");
+        }
+
+        req.token = token;
+        req.userId = user._id;
+        req.user = user;
+
+        next();
+
+    }
+    catch (err) {
+        // console.log(err);
+        res.status(401).json({
+            message: err.message,
+            error: err,
+            status: false
+        });
+    }
+}
+
+module.exports = adminAuth;
--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authenticate = require('../middleware/auth');
+const adminAuth = require('../middleware/adminAuth');
 const { register, login, auth, logout } = require('../controllers/userController');
 const { manageStock, getStock } = require('../controllers/inventoryController');
 const { donorRequest, patientRequest } = require('../controllers/historyController');
@@ -13,14 +14,14 @@ router.get('/user/authenticate', authenticate, auth);
 router.get('/user/logout/:id', logout);
 
 // inventory routes
-router.post('/inventory/manage-stock', manageStock);
-router.get('/inventory/get-stock', getStock);
+router.post('/inventory/manage-stock', adminAuth, manageStock);
+router.get('/inventory/get-stock', adminAuth, getStock);
 
 // history routes
-router.post('/donor/:type', donorRequest);
-router.post('/patient', patientRequest);
+router.post('/donor/:type', authenticate, donorRequest);
+router.post('/patient', authenticate, patientRequest);
 
 // admin routes
-router.post('/admin/:user/:type/:status', adminControls);
+router.post('/admin/:user/:type/:status', adminAuth, adminControls);
 
 module.exports = router;
