a8faadbedada006e0196da42b625b645a797c911
Poojasriramineni <poojasri4399@gmail.com>
2024-01-21 15:53:48 -0600

    action column added to patients
    action column added to patients

--- client/src/pages/homePage/Patient.jsx
+++ client/src/pages/homePage/Patient.jsx
@@ -3,15 +3,16 @@ import React, { useCallback, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import LoadingSpinner from '../../components/LoadingSpinner';
 import { patientsListRoute } from '../../utils/ApiRoutes';
-import { Container } from 'react-bootstrap';
-import { patientColumns } from '../../utils/tableHeaders/patientHeaders';
+import { Button, Container } from 'react-bootstrap';
 import ReactTable from '../../components/ReactTable';
+import { donorPatientHeaders } from '../../utils/tableHeaders/donorPatinetHeaders';
 
 export const Patient = () => {
 
   const navigate = useNavigate();
   const [patientList, setPatientList] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [columns, setColumns] = useState(donorPatientHeaders);
 
   const getPatients = useCallback ( async () => {
     try {
@@ -40,6 +41,28 @@ export const Patient = () => {
   useEffect(() => {
     getPatients();
   }, [getPatients, navigate]);
+
+  useEffect(() => {
+    setColumns([...columns,{
+      dataField: "Actions",
+      isDummyField: true,
+      text: "Actions",
+      headerStyle: {
+        backgroundColor: "#DEDADA",
+        width: '10%'
+      },
+      formatter: (cell, row) => {
+        return (  <>
+          <Button
+            variant={row.status === 'rejected' ? 'success' : 'danger'}
+            size="sm">
+            {row.status === 'rejected' ? 'Accept' : 'Reject'}
+          </Button>
+        </>
+      )}
+    }])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
  
   return (
     <Container className="h-100">
@@ -51,7 +74,7 @@ export const Patient = () => {
               title={'Patients waiting for Transfusion'}
               pageSize={8}
               data={patientList}
-              columns={patientColumns}
+              columns={columns}
             />
         )
       }
