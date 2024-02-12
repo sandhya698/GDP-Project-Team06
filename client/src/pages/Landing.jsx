5c25478bbf8aa9acc64fd77d4d2213bf7989f1b7
praveennarni <praveenbabunarni1999@gmail.com>
2024-02-12 09:39:57 -0600

    donate button with basic modal

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavigationBar from '../components/NavigationBar'
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Button, Col, Modal } from 'react-bootstrap';
 
 
 export default function Landing() {
+  const [modalShow, setModalShow] = useState(false);
+  const MyVerticallyCenteredModal = (props) => {
+    return (
+      <Modal
+        {...props}
+        size="md"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header className='px-4 py-2' closeButton>
+          <Modal.Title  id="contained-modal-title-vcenter">
+          Modal Title
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant='danger' onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
   return (
     <>
       <Container fluid className='landing-page' >
@@ -14,7 +42,18 @@ export default function Landing() {
               <h1 className='landing-title'>donate blood today!</h1>
               <h3 className='landing-slogan'> </h3>
               <p className='landing-qoute'>In the tapestry of humanity, your blood becomes a thread of compassion, weaving stories of survival and strength. Donate generously, for in each drop lies the power to heal and unite us in the symphony of life's resilience.</p>
+              <Button
+                className='donate-button px-3 py-1'
+                variant="outline-danger"
+                style={{ borderRadius: 0, borderWidth: "2px" }}
+                onClick={() => setModalShow(true)}>
+                Donate
+              </Button>
             </Col>
+            <MyVerticallyCenteredModal
+              show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
           </Row>
         </Container>
       </Container>
