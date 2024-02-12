089eaac51b9406a94c3e548c48193095fef1bc7d
S560460 <S560460@nwmissouri.edu>
2024-02-12 08:13:23 -0600

    added developer info to about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavigationBar from '../components/NavigationBar'
+import { Card, Col, Container, Image, Row } from 'react-bootstrap';
+import { developers } from '../utils/developers';
 
 export const About = () => {
+  
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+
   return (
     <>
-       <NavigationBar />   
-      <div>About Page</div>
+      <NavigationBar
+        isAuthenticated={isAuthenticated}
+      />
+      <Container fluid >
+        <Row className="full-height-row ">
+          <Col sm={12} md={10} className='mx-auto my-4'>
+            <Row className="overflow-x-auto">
+              <h4 className="mb-3 text-capitalize">Developers</h4>
+              {
+                developers.map((data, index) => {
+                  return (
+                    <Col key={index}>
+                      <Card  className='dev-card shadow gx-0'>
+                        <Card.Header className='dev-header text-center' >
+                          <Image src={data.img} roundedCircle />
+                        </Card.Header>
+                        <Card.Body className='mt-4'>
+                          <Card.Title className='text-center' >{data.name}</Card.Title>
+                          <Card.Text className='text-center' >{data.about}</Card.Text>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                    
+                  )
+                })
+              }
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
---
+++ client/src/utils/developers.js
@@ -0,0 +1,32 @@
+export const developers = [
+    {
+        name: "Sandhya Paladugu",
+        img: '/img.jpeg',
+        about: "1.6 years of experience as a system administrator at DXC Technology. Expertise in Citrix VDI administration."
+    },
+    {
+        name: "Manoj Vamanaguntla",
+        img: '/img.jpeg',
+        about: "4 years of experience in Data Anaystat IpowerFour Technologies. Expertise in Database and, Quality testin."
+    },
+    {
+        name: "Aparna Marepally",
+        img: '/img.jpeg',
+        about: "Passionate to excel in the field of full stack developer, Startups and entrepreneurship."
+    },
+    {
+        name: "Praveen Babu",
+        img: '/img.jpeg',
+        about: "Achieved AWS (Amazon Web Services) and Python Certifications."
+    },
+    {
+        name: "Pooja Sri Ramineni",
+        img: '/img.jpeg',
+        about: "Salesforce tester, expertise in error reporting and prioritizing them in JIRA"
+    },
+    {
+        name: "Vijaya Gopal Raju",
+        img: '/img.jpeg',
+        about: "Good Skills in Salesforce. Good at team participation and time management."
+    },
+];
