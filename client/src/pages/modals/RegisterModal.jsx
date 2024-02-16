e7a72c6cdf99891b8e32162b3ec1da973b6fab98
Poojasriramineni <poojasri4399@gmail.com>
2024-02-16 08:47:02 -0600

    login functionality added to register modal

--- client/src/pages/modals/RegisterModal.jsx
+++ client/src/pages/modals/RegisterModal.jsx
@@ -1,25 +1,118 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Col, FloatingLabel, Form, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { toastOptions } from '../../utils/toasOptions';
+import { toast } from 'react-toastify';
+import axios from 'axios';
+import { registerRoute } from '../../utils/ApiRoutes';
 
-export default function RegisterModal({handleUser}) {
+export default function RegisterModal({ handleUser }) {
+
+  const [userDetails, setUserDetails] = useState({
+    name: "", email: "", password: "", cpassword: "", userType: ""
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    return setUserDetails({ ...userDetails, [name]: value });
+  }
+
+  const validateUserDetails = () => {
+    const { name, email, password, cpassword, userType } = userDetails;
+    console.log(userDetails);
+
+    let validated = true;
+
+    const nameRegex = /^[a-zA-z0-9_ ]{3,25}$/;
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[.!@#$%^&*-])[a-zA-Z\d.!@#$%^&*-]{8,16}$/;
+
+    toast.dismiss();
+
+    if (!nameRegex.test(name)) {
+      toast.error(
+        "Username should be greater than 3 chars. Special chars allowed space and underscore",
+        toastOptions
+      );
+      validated = false;
+    }
+
+    if (!emailRegex.test(email)) {
+      toast.error(
+        "Invalid email",
+        toastOptions
+      );
+      validated = false;
+    }
+
+    if (!passwordRegex.test(password)) {
+      toast.error(
+        "Password should be min of 8 and max 16. Should follow standard password rules",
+        toastOptions
+      );
+    validated = false;
+    }
+
+    if (!password.match(cpassword)) {
+      toast.error(
+        "Passwords does not match",
+        toastOptions
+      );
+      validated = false;
+    }
+
+    if (userType === '') {
+      toast.error(
+        "Who are you?",
+        toastOptions
+      );
+      validated = false;
+    }
+
+    return validated;
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const { name, email, password, cpassword, userType } = userDetails;
+
+    if (validateUserDetails()) {
+      try {
+        const res = await axios.post(registerRoute, {
+          name, email, password, cpassword, userType
+        });
+  
+        console.log(res);
+        if(res.data.success) {
+          toast.success('You are now a Transfuser!!', toastOptions);
+          handleUser(true);
+        };
+      }
+      catch (err) {
+        console.log(err.response.data);
+        toast.error(err.response.data.message, toastOptions);
+      }
+      
+		}
+  }
+
   return (
     <>
       <Row className="d-flex justify-content-center align-items-center">
         <h2 className="fw-bold mb-2 text-uppercase">transfuse now</h2>
         <p className="mb-3">Please enter your details to become a Transfuser!</p>
-        <Form className="mb-3" >
+        <Form className="mb-3" onSubmit={handleSubmit}>
             
           <Form.Group className="mb-3">
             <FloatingLabel controlId="name" label="Name" >
-              <Form.Control className='border-2' type="text" placeholder="Dream Fuel" name="name" required />
+              <Form.Control className='border-2' type="text" placeholder="Dream Fuel" name="name" onChange={handleChange} required />
             </FloatingLabel> 
           </Form.Group>
 
           <Row className="mb-3 align-items-center">
             <Form.Group className="mb-3" as={Col}>
               <FloatingLabel controlId="email" label="Email address"  >
-                <Form.Control className='border-2' type="email" placeholder="name@example.com" name="email"  required />
+                <Form.Control className='border-2' type="email" placeholder="name@example.com" name="email" onChange={handleChange}  required />
               </FloatingLabel> 
             </Form.Group>
             
@@ -27,9 +120,9 @@ export default function RegisterModal({handleUser}) {
               <div className="d-flex flex-column  align-items-start flex-md-row justify-content-md-around">
                 <Form.Label className="text-center"> I am a </Form.Label>
                 <Form.Check type="radio" value="donor" label="Donor" name="userType" id="donor"
-                    />
+                  checked={userDetails.userType === "donor"} onChange={handleChange} />
                 <Form.Check type="radio" value="patient" label="Patient" name="userType" id="patient"
-                    />
+                  checked={userDetails.userType === "patient"} onChange={handleChange} />
               </div>
             </Form.Group>
           </Row>
@@ -37,13 +130,13 @@ export default function RegisterModal({handleUser}) {
           <Row className="mb-2">
             <Form.Group className="mb-3" as={Col}>
               <FloatingLabel controlId="password" label="Password" >
-                <Form.Control className='border-2' type="password" placeholder="secret" name="password" required />
+                <Form.Control className='border-2' type="password" placeholder="secret" name="password" onChange={handleChange} required />
               </FloatingLabel> 
             </Form.Group>
 
             <Form.Group className="mb-3" as={Col}>
               <FloatingLabel controlId="cpassword" label="Confirm Password" >
-                <Form.Control className='border-2' type="password" placeholder="secret" name="cpassword" required />
+                <Form.Control className='border-2' type="password" placeholder="secret" name="cpassword" onChange={handleChange} required />
               </FloatingLabel> 
             </Form.Group>
           </Row>
