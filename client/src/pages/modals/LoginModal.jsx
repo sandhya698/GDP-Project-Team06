f645a9d711cadf31d6e1f9eb9668a41184290c26
Poojasriramineni <poojasri4399@gmail.com>
2024-02-16 08:43:15 -0600

    login functionality added to login modal

--- client/src/pages/modals/LoginModal.jsx
+++ client/src/pages/modals/LoginModal.jsx
@@ -1,24 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Card, Form, FloatingLabel, Row } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify';
+import { toastOptions } from '../../utils/toasOptions';
+import axios from 'axios';
+import { loginRoute } from '../../utils/ApiRoutes';
 
-export default function LoginModal({handleUser}) {
+export default function LoginModal({ handleUser }) {
+
+  const navigate = useNavigate();
+
+  const [loginDetails, setLoginDetails] = useState({
+    email: "", password: ""
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    return setLoginDetails({ ...loginDetails, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log('in handle submit')
+    const { email, password } = loginDetails;
+
+    if (!email || !password) {
+      toast.error("email and password are required", toastOptions);
+    }
+    else {
+      try {
+        const res = await axios.post(loginRoute,
+          { email, password },
+          { withCredentials: true }
+        );
+        
+        // console.log(res);
+        if (res.data.success) {
+          toast.success('Successfully logged In', toastOptions);
+          navigate('/home');
+        }
+        
+      }
+      catch (err) {
+        console.log(err.response.data);
+        toast.error(err.response.data.message, toastOptions);
+        // window.alert(err.response.data.message);
+      }
+    }
+  }
+
   return (
     <>
       <Row className="d-flex justify-content-center align-items-center p-2">
         <h2 className="fw-bold mb-2 text-uppercase">Transfuse now</h2>
         <p className="mb-3">Login to start Transfusion!</p>
           <Card.Body>
-            <Form className="mb-3" >
+            <Form className="mb-3" onSubmit={handleSubmit} >
               <Form.Group className="mb-3">
                 <FloatingLabel controlId="email" label="Email address"  >
-                  <Form.Control className='border-2' type="email" placeholder="name@example.com" name="email"  required />
+                  <Form.Control className='border-2' type="email" placeholder="name@example.com" name="email" onChange={handleChange} required />
                 </FloatingLabel> 
               </Form.Group>
                 
               <Form.Group className="mb-3">
                 <FloatingLabel controlId="password" label="Password" >
-                  <Form.Control className='border-2' type="password" placeholder="secret" name="password"  required />
+                  <Form.Control className='border-2' type="password" placeholder="secret" name="password" onChange={handleChange} required />
                 </FloatingLabel> 
               </Form.Group>
 
@@ -29,7 +75,7 @@ export default function LoginModal({handleUser}) {
               </div>
 
               <div className="d-grid">
-                <Button variant="danger">
+                <Button variant="danger" type="submit">
                   Start Transfusion
                 </Button>
               </div>
