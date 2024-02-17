9c9779999f60f17a09e138c7eaf2bcd029bdf69f
S560460 <S560460@nwmissouri.edu>
2024-02-16 20:34:47 -0600

    added routes to all pages and updated Home page

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,11 @@ import { About } from './pages/About';
 import { Faq } from './pages/Faq';
 import Landing from './pages/Landing';
 import { GlobalStateProvider } from './reducer/GlobalState';
+import { Donor } from './pages/homePage/Donor';
+import { Patient } from './pages/homePage/Patient';
+import { Donations } from './pages/homePage/Donations';
+import { Requests } from './pages/homePage/Requests';
+import Inventory from './pages/homePage/Inventory';
 
 export default function App() {
 
@@ -17,13 +22,18 @@ export default function App() {
             <GlobalStateProvider>
                 <BrowserRouter>
                     <Routes>
-                        <Route path='/'         element={<Landing />} />
-                        <Route path='/about'    element={<About />} />
-                        <Route path='/login'    element={<Login />} />
-                        <Route path='/register' element={<Register />} />
-                        <Route path='/faq'      element={<Faq />} />
-                        <Route path='/home'     element={<Home />} />
-                        <Route path='*'         element={<Error />} />
+                        <Route path='/'          element={<Landing />} />
+                        <Route path='/about'     element={<About />} />
+                        <Route path='/login'     element={<Login />} />
+                        <Route path='/register'  element={<Register />} />
+                        <Route path='/faq'       element={<Faq />} />
+                        <Route path='/home'      element={<Home />} />
+                        <Route path='/donors'    element={<Donor />} />
+                        <Route path='/patients'  element={<Patient />} />
+                        <Route path='/donations' element={<Donations />} />
+                        <Route path='/requests'  element={<Requests />} />
+                        <Route path='/Inventory' element={<Inventory />} />
+                        <Route path='*'          element={<Error />} />
                     </Routes>
                 </BrowserRouter>
                 <ToastContainer />
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React, { useCallback, useEffect, useState } from 'react'
-import NavigationBar from '../components/NavigationBar'
 import axios from 'axios';
 import { authenticate } from '../utils/ApiRoutes';
 import { useNavigate } from 'react-router-dom';
@@ -7,18 +6,11 @@ import { Col, Container, Row } from 'react-bootstrap';
 import { Sidebar } from '../components/Sidebar';
 import Dashboard from './homePage/Dashboard';
 import LoadingSpinner from '../components/LoadingSpinner';
-import { Donor } from './homePage/Donor';
-import { Patient } from './homePage/Patient';
-import { Donations } from './homePage/Donations';
-import { Requests } from './homePage/Requests';
-import Inventory from './homePage/Inventory';
 
 export const Home = () => {
 
-  const [currentUser, setCurrentUser] = useState(undefined);
   const [loading, setLoading] = useState(true);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [currentPage, setCurrentPage] = useState('home');
   const navigate = useNavigate();
 
   const checkUserAuthentication = useCallback(async () => {
@@ -31,7 +23,6 @@ export const Home = () => {
         }
       });
       console.log(res.data);
-      setCurrentUser(res.data.user);
       setIsAuthenticated(true);
     }
     catch (err) {
@@ -41,7 +32,7 @@ export const Home = () => {
       }
     }
     finally {
-      setLoading(false);
+        setLoading(false);
     }
   }, [navigate]);
 
@@ -49,42 +40,6 @@ export const Home = () => {
     checkUserAuthentication();
   }, [checkUserAuthentication]);
 
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-  }
-
-  const PageToRedner = () => {
-    if (currentPage === 'home') {
-      return (
-        <Dashboard />
-      )
-    }
-    else if (currentPage === 'patient'){
-      return (
-        <Patient />
-      )
-    }
-    else if (currentPage === 'inventory') {
-      return (
-        <Inventory />
-      )
-    }
-    else if (currentPage === 'donations'){
-      return (
-        <Donations />
-      )
-    }
-    else if (currentPage === 'donor'){
-      return (
-        <Donor />
-      )
-    }
-    else if (currentPage === 'bloodrequest'){
-      return (
-        <Requests />
-      )
-    }
-  }
   
   return (
     <>
@@ -98,12 +53,10 @@ export const Home = () => {
               <Container fluid >
                 <Row className="full-height-row">
                   <Col sm={2} md={3} lg={2} className='p-0'>
-                    <Sidebar
-                      userType={currentUser.userType}
-                      pageChange={handlePageChange}/>
+                    <Sidebar/>
                   </Col>
                   <Col sm={10} md={9} lg={10} className='p-0'>
-                    <PageToRedner />
+                    <Dashboard />
                   </Col>
                 </Row>
               </Container>
