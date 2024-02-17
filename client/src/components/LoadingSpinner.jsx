ba7a448f539a6ddc55d55e40d1fd337cb601ec21
CH V V GOPAL RAJU KALIDINDI <S559007@NWMISSOURI.EDU>
2024-02-17 00:16:16 -0600

    updated loading spinner

--- client/src/components/LoadingSpinner.jsx
+++ client/src/components/LoadingSpinner.jsx
@@ -1,12 +1,81 @@
 import React from 'react'
-import { Container, Spinner } from 'react-bootstrap';
+import styled from "styled-components";
 
 export default function LoadingSpinner() {
   return (
-    <Container className='d-flex justify-content-center align-items-center' style={{height: "100vh"}}>
-      <Spinner animation="border" variant="primary">
-        <span className="visually-hidden">Loading...</span>
-      </Spinner>
-    </Container>
+    <Loader>
+       <div className="main">
+        <div className="load">
+            <div className="circle"></div>
+            <h1>~loading~</h1>
+        </div>
+    </div>
+    </Loader>
   );
 };
+
+const Loader = styled.div`
+  @import url("https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap");
+  .main{
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .load{
+    width: 15rem;
+    height: 15rem;
+    position: relative;
+    border-radius: 50%;
+  }
+
+  .circle{
+    width: 15rem;
+    height: 15rem;
+    position: absolute;
+    border-right: .5rem solid red;
+    border-radius: 50%;
+    animation: load 1s linear infinite;
+  }
+
+  h1{
+    color: #000;
+    font-size: 2rem;
+    letter-spacing: 0.2rem;
+    font-weight: 500;
+    position: absolute;
+    top: 50%; left: 50%;
+    transform: translate(-50%,-50%);
+    font-family: 'Fredoka One', cursive;
+  }
+
+  @keyframes load {
+    0%{
+        transform: rotate(0deg);
+    }
+    100%{
+        transform: rotate(360deg);
+    }
+  }
+
+  @media (max-width: 490px) {
+    .load, .circle {
+      width: 12rem;
+      height: 12rem;
+    }
+
+    .circle {
+      width: 12rem;
+      height: 12rem;
+      border-right: .5rem solid red;
+    }
+
+    h1 {
+      font-size: 1.6rem;
+      font-weight: 600
+    }
+
+  }
+          
+`;
