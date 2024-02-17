bb662a35e63d0ebf5669771be0ec94a2b1fef9ef
sandhya paladugu <sandhyachoudhary698@gmail.com>
2024-02-16 19:59:44 -0600

    manage global state on refresh

--- client/src/reducer/GlobalState.js
+++ client/src/reducer/GlobalState.js
@@ -1,22 +1,36 @@
-import React, { createContext, useContext, useReducer } from 'react';
+import { createContext, useContext, useReducer, useEffect } from 'react';
 
 const GlobalStateContext = createContext();
 
+const initialState = {
+  user: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_USER':
+      return { ...state, user: action.payload };
+    case 'RESTORE_STATE':
+      return { ...state, ...action.payload };
+    default:
+      return state;
+  }
+};
+
 const GlobalStateProvider = ({ children }) => {
-  const initialState = {
-    user: null,
-  };
-
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case 'SET_USER':
-        return { ...state, user: action.payload };
-      default:
-        return state;
-    }
-  };
-
+  
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    const storedState = JSON.parse(localStorage.getItem('globalState'));
+    if (storedState) {
+      dispatch({ type: 'RESTORE_STATE', payload: storedState });
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('globalState', JSON.stringify(state));
+  }, [state]);
 
   return (
     <GlobalStateContext.Provider value={{ state, dispatch }}>
